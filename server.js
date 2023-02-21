bafb4318451fb35b9b319862895de30477fb6cbe
Geonwoo Park <parkkeonwo@naver.com>
2023-02-21 16:41:38 +0900

    230221 16:41

--- server.js
+++ server.js
@@ -39,16 +39,17 @@ app.get('/pwapj1/mainpage',(req, res)=>{
 const ogs = require('open-graph-scraper');
 
 app.post('/ogs',(req, res)=>{
-  res.send(req.body);
 
-  // const options = { url: 'http://ogp.me/' };
-  // ogs(options)
-  //   .then((data) => {
-  //     const { error, result, response } = data;
-  //     console.log('error:', error);  // This returns true or false. True if there was an error. The error itself is inside the results object.
-  //     console.log('result:', result); // This contains all of the Open Graph results
-  //     console.log('response:', response); // This contains the HTML of page
-  //   })
+  const options = { url: req.body.url };
+  ogs(options)
+    .then((data) => {
+      const { error, result, response } = data;
+      console.log('error:', error);  // This returns true or false. True if there was an error. The error itself is inside the results object.
+      console.log('result:', result); // This contains all of the Open Graph results
+      console.log('response:', response); // This contains the HTML of page
+      res.send(result);
+    })
+
 })
 
 
